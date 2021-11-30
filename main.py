3a9b2c68c0237f6b48fa77d95de6a447e8394c01
hamza-sayyid <94769692+hamza-sayyid@users.noreply.github.com>
2021-11-30 13:51:58 -0800

    Bit of refactoring

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import numpy as np
-from basic import basic, matchings, alignment, validate
+from basic import basic
 from time import perf_counter
-from pathlib import Path
 import tracemalloc
 
 # from sequence_alignment1 import alignment
-filename = "BaseTestcases_CS570FinalProject/input4.txt"
+filename = "BaseTestcases_CS570FinalProject/input1.txt"
 
 def generate_strings(base_strings):
     """
@@ -53,16 +51,17 @@ def main():
     strings = generate_strings(base_strings)
     print(f"Generated cumulative strings: {strings}\n")
 
-    s1, s2 = strings
 
+    s1, s2 = strings
     tracemalloc.start()
+
     start = perf_counter()
     
-    A, opt = basic(s1, s2)
-    opt_matching = matchings(A, s1, s2, len(s1), len(s2))
-    final_s1, final_s2 = alignment(s1, s2, opt_matching[::-1]+[[len(s1),len(s2)]])
+    opt, final_s1, final_s2 = basic(s1, s2)
 
     end = perf_counter()
+
+
     num = filename.split("/")[-1][5:-4]
     with open(f'output{num}.txt','w') as f:
         f.write(final_s1[:50] + " " + final_s1[-50:] + "\n")
